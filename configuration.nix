fbf9bf6a000b2064444c389b220d5aade99ee839
Robert F. Nash <robert@robertnash.net>
2014-07-04 10:43:46 -0400

    changed formatting

--- configuration.nix
+++ configuration.nix
@@ -7,15 +7,15 @@
 
   boot = {
     loader.grub = {
-       enable = true;
-       version = 2;
-       device = "/dev/sda";
+      enable = true;
+      version = 2;
+      device = "/dev/sda";
     };
     initrd = {
-      luks.devices = [ { 
+      luks.devices = [ {
         device = "/dev/sda3";
         name = "gentoo_root";
-	preLVM = true;
+        preLVM = true;
       } ];
       supportedFilesystems = [ "zfs"];
     };
@@ -30,73 +30,74 @@
       allowedUDPPorts = [80 443 9001 9030 31329 48286 51413 15899 2222];
     };
   };
-  
-  fileSystems = [ 
-    { mountPoint = "/";
-      label = "nix-btrfs";
-      fsType = "ext4";
-    }
-    {
-      mountPoint = "/home";
-      label = "home";
-      fsType = "ext4";
-    }
-    {
-      mountPoint = "/tmp";
-      device = "tmpfs";
-      fsType = "tmpfs";
-      options = "size=2G";
-    }
+
+  fileSystems = [
+  {
+    mountPoint = "/";
+    label = "nix-btrfs";
+    fsType = "ext4";
+  }
+  {
+    mountPoint = "/home";
+    label = "home";
+    fsType = "ext4";
+  }
+  {
+    mountPoint = "/tmp";
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = "size=2G";
+  }
   ];
 
   swapDevices = [ { device = "/dev/vg_rfnashlaptop/lv_swap"; } ];
 
   i18n = {
-     consoleFont = "lat9w-16";
-     consoleKeyMap = "dvorak";
-     defaultLocale = "en_US.UTF-8";
-   };
+    consoleFont = "lat9w-16";
+    consoleKeyMap = "dvorak";
+    defaultLocale = "en_US.UTF-8";
+  };
 
-   services = {
-     # openssh.enable = true;
-     printing.enable = true;
-     unbound.enable = true;  
-     xserver = {
-       enable = true;
-       layout = "dvorak";
-       xkbOptions = "lv3:ralt_switch,ctrl:swapcaps,compose:ralt,terminate:ctrl_alt_bksp";
-       desktopManager.xterm.enable = false;
-       modules = [ "xf86-input-synaptics" ];
-       synaptics = {
-         enable = true;
-	 maxSpeed = "0.8";
-	 twoFingerScroll = true;
-       };
-     };  
-     httpd = {
-       enable = true;
-       enableUserDir = true;
-       adminAddr = "rfnash@localhost";
-       extraModules =  [ { name = "php5"; path = "/nix/store/j0ihspf2zhkdspsd9n7ma8zn19macpkm-php-5.3.18/modules/libphp5.so"; } ];
-     };
-     gogoclient = {
-       enable = true;
-       server = "montreal.freenet6.net";
-       username = "rfnash";
-       password = "/etc/nixos/gogoc.passwd";
-     };
-     dovecot2 = {
-       enable = true;
-       enablePop3 = false;
-       mailLocation = "maildir:~/Maildir:LAYOUT=fs:INBOX=~/Maildir/Inbox";
-     };
-   };
+  services = {
+#   openssh.enable = true;
+    printing.enable = true;
+    unbound.enable = true;
+    xserver = {
+      enable = true;
+      layout = "dvorak";
+      xkbOptions = "lv3:ralt_switch,ctrl:swapcaps,compose:ralt,terminate:ctrl_alt_bksp";
+      desktopManager.xterm.enable = false;
+      modules = [ "xf86-input-synaptics" ];
+      synaptics = {
+        enable = true;
+        maxSpeed = "0.8";
+        twoFingerScroll = true;
+      };
+    };
+    httpd = {
+      enable = true;
+      enableUserDir = true;
+      adminAddr = "rfnash@localhost";
+      extraModules =  [ { name = "php5"; path = "/nix/store/j0ihspf2zhkdspsd9n7ma8zn19macpkm-php-5.3.18/modules/libphp5.so"; } ];
+    };
+    gogoclient = {
+      enable = true;
+      server = "montreal.freenet6.net";
+      username = "rfnash";
+      password = "/etc/nixos/gogoc.passwd";
+    };
+    dovecot2 = {
+      enable = true;
+      enablePop3 = false;
+      mailLocation = "maildir:~/Maildir:LAYOUT=fs:INBOX=~/Maildir/Inbox";
+    };
+  };
 
   security.sudo.wheelNeedsPassword = false;
   users.defaultUserShell = "/run/current-system/sw/bin/zsh";
   time.timeZone = "America/Detroit";
   hardware.pulseaudio.enable = true;
-  
+
   environment = {
     enableBashCompletion = true;
     systemPackages = with pkgs; [
@@ -126,16 +127,16 @@
       xclip
       xsel
       zsh
-     ];
+      ];
 
-     x11Packages = with pkgs; [
-       rxvt_unicode
-     ];
+    x11Packages = with pkgs; [
+      rxvt_unicode
+      ];
   };
 
   nixpkgs.config = {
     rxvt_unicode = {
-       perlSupport = true;
+      perlSupport = true;
     };
 
     vim = {
@@ -147,15 +148,15 @@
     enableFontDir = true;
     enableGhostscriptFonts = true;
     extraFonts = [
-       pkgs.anonymousPro
-       pkgs.corefonts
-       pkgs.dejavu_fonts
-       pkgs.gentium
-       pkgs.liberation_ttf
-       pkgs.libertine
-       pkgs.lmodern
-       pkgs.terminus_font
-       pkgs.ttf_bitstream_vera
-    ];
+      pkgs.anonymousPro
+      pkgs.corefonts
+      pkgs.dejavu_fonts
+      pkgs.gentium
+      pkgs.liberation_ttf
+      pkgs.libertine
+      pkgs.lmodern
+      pkgs.terminus_font
+      pkgs.ttf_bitstream_vera
+      ];
   };
 }
